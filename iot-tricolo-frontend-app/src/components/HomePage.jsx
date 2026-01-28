a45935747e63a2b50b5768a65b451b663b9e4205
Bradley Leneus <6243042@cegepmv.ca>
2026-01-28 15:48:33 -0500

    Test push 3 render

--- iot-tricolo-frontend-app/src/components/HomePage.jsx
+++ iot-tricolo-frontend-app/src/components/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
 
             <div>
 
-                 <p>Déploiement du frontend 2</p>
+                 <p>Déploiement du frontend 6 7 </p>
          
             </div>
         
